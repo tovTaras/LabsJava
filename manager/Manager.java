45841b8d74e6f5599f043fc8fa2557189646d3f1
tovTaras <tovtarik@ukr.net>
2022-04-14 01:50:55 +0300

    LAB3

--- manager/Manager.java
+++ manager/Manager.java
@@ -1,13 +1,16 @@
 package ua.lviv.iot.lab2.manager;
 import ua.lviv.iot.lab2.hierarchy.Materials;
-import java.util.ArrayList;
-
+import java.util.*;
 
 public class Manager {
     private Materials[] arrOfMaterials;
     ArrayList<String> toDoList = new ArrayList<String>();
     public Manager(Materials[] arrOfMaterials){
         this.arrOfMaterials=arrOfMaterials;
+        sortArrayByPricePerKilo();
+        sortArrayByQuantityInKg();
+        sortArrayByPricePerKiloReflected();
+        sortArrByQuantityInKgReflected();
         createRenovationPlan();
         printToDoList();
     }
@@ -23,4 +26,46 @@ public class Manager {
         }
         System.out.println("End.");
     }
+    Comparator PricePerKiloComparator= Comparator.comparing(Materials::getPricePerKilo);
+    Comparator QuantityInKgComparator = Comparator.comparing(Materials::getQuantityInKg);
+    public void sortArrayByPricePerKilo(){
+        Arrays.sort(arrOfMaterials, PricePerKiloComparator);
+        System.out.println("Sorted by Price: ");
+        for (int i = 0; i < arrOfMaterials.length; i++)
+        {
+            System.out.println(arrOfMaterials[i].toString());
+        }
+        System.out.println();
+
+    }
+    public void sortArrayByPricePerKiloReflected(){
+        Arrays.sort(arrOfMaterials, PricePerKiloComparator.reversed());
+        System.out.println("Sorted(R) by Price: ");
+        for (int i = 0; i < arrOfMaterials.length; i++)
+        {
+            System.out.println(arrOfMaterials[i].toString());
+        }
+        System.out.println();
+
+    }
+    public void sortArrayByQuantityInKg(){
+        Arrays.sort(arrOfMaterials, QuantityInKgComparator);
+        System.out.println("Sorted by Quantity: ");
+        for (int i = 0; i < arrOfMaterials.length; i++)
+        {
+            System.out.println(arrOfMaterials[i].toString());
+        }
+        System.out.println();
+
+    }
+    public void sortArrByQuantityInKgReflected(){
+        Arrays.sort(arrOfMaterials, QuantityInKgComparator.reversed());
+        System.out.println("Sorted(R) by Quantity: ");
+        for (int i = 0; i < arrOfMaterials.length; i++)
+        {
+            System.out.println(arrOfMaterials[i].toString());
+        }
+        System.out.println();
+    }
+
 }
