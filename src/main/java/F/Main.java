ab976d305a4f513380bed16875954e80da6b7645
tovTaras <tovtarik@ukr.net>
2022-05-05 23:19:08 +0300

    lab-6 is here

---
+++ src/main/java/F/Main.java
@@ -0,0 +1,9 @@
+package F;
+
+import ua.lviv.iot.lab2.manager.RenovationCompany;
+
+public class Main {
+    public static void main(String[] args) {
+        RenovationCompany renovationCompany_one = new RenovationCompany();
+    }
+}
